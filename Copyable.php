afb23d328a819f9aad2f87b7b8a5e86d151900a7
pater4 <pater4.1127+patera@gmail.com>
2023-09-09 10:25:06 +0900

    phpstanのエラー修正

--- Copyable.php
+++ Copyable.php
@@ -2,7 +2,9 @@
 
 trait Copyable
 {
-    function copy(...$params)
+    // 誤った推測をするので無視する
+    /** @phpstan-ignore-next-line */
+    function copy(...$params): static
     {
         $prev = $this->copyDefalutValues();
         $new = array_intersect_key($params, $prev);
@@ -10,10 +12,14 @@ trait Copyable
         return new static(...[...$prev, ...$new]);
     }
 
-    function copyDefalutValues(): array
+    /**
+     * @return array<string, mixed>
+     */
+    function copyDefalutValues()
     {
-        $rf = new ReflectionClass(self::class);
-        $filter = array_fill_keys(array_map(fn ($v) => $v->name, $rf->getConstructor()->getParameters()), 0);
+        $rf = new \ReflectionClass(self::class);
+        $names = array_map(fn ($v) => $v->name, $rf->getConstructor()?->getParameters() ?? []);
+        $filter = array_fill_keys($names, 0);
         $defValues = (array)$this;
         return array_intersect_key($defValues, $filter);
     }
